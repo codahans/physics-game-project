4a7f6b167083e5e83292f5b575c0bc8b75788e57
codahans <21coop21@gmail.com>
2022-05-12 13:41:59 -0400

    user input for charges added

--- js/AngryBird.js
+++ js/AngryBird.js
@@ -1,87 +1,103 @@
 const canvas = document.getElementById("AngryBird");
 const ourWorld = window.boxbox.createWorld(canvas);
+const offsetX = 30.60;
+const offsetY = 27.60;
+drawCanvas();
 var inputflag = 1;
 var chargeflag = 1;
 
-ourWorld.createEntity({
-  name: "bird",
-  shape: "circle",
-  image: "negative-electron.png",
-  radius: 1,
-  imageStretchToFit: true,
-  density: 4,
-  x: 1.5,
-  y: 11,
-  onKeyDown: function (e) {
-    if (e.key === " " && inputflag) {
-      this.applyImpulse(120, 60);
-      console.log(e);
-      inputflag = 0;
-    }
-  },
-  onImpact: function (entity, force) {
-    if (entity.name() === "positive-charge") {
-      this.applyImpulse(90, 65);
-    }
-    if (entity.name() === "target") {
-      this._world._destroy(this);
-    }
-    if (entity.name() === "ground") {
-      this._world._destroy(this);
-    }
-  },  
-});
+function drawCanvas() {
+  var ctx = canvas.getContext("2d");
+  ctx.canvas.width  = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
 
-ourWorld.createEntity({
-  name: "ground",
-  shape: "square",
-  type: "static",
-  color: "rgb(0,0,0)",
-  width: 42,
-  height: 0.5,
-  y: 12.5,
-  x: 1,
-});
-
-ourWorld.createEntity({
-  name: "spawnpoint",
-  shape: "square",
-  type: "static",
-  color: "red",
-  width: 4,
-  height: 0.5,
-  y: 12,
-  x: 1,
-});
-
-const block = {
-  name: "target",
-  shape: "square",
-  type: "static",
-  color: "pink",
-  width: 4,
-  height: 0.4,
-  onImpact: function (entity, force) {
-    if (entity.name() === "bird") {
-      this.color("green");
-    }
-  },
-};
-
-function placeCharge() {
   ourWorld.createEntity({
-    name: "positive-charge",
+    name: "ground",
+    shape: "square",
+    type: "static",
+    color: "rgb(0,0,0)",
+    width: 42,
+    height: 0.5,
+    y: 12.5,
+    x: 1,
+  });
+
+  ourWorld.createEntity({
+    name: "spawnpoint",
+    shape: "square",
+    type: "static",
+    color: "red",
+    width: 4,
+    height: 0.5,
+    y: 12,
+    x: 1,
+  });
+
+  ourWorld.createEntity({
+    name: "projectile",
+    shape: "circle",
+    image: "negative-electron.png",
+    radius: 1,
+    imageStretchToFit: true,
+    density: 4,
+    x: 1.5,
+    y: 11,
+    onKeyDown: function (e) {
+      if (e.key === " " && inputflag) {
+        this.applyImpulse(120, 60);
+        inputflag = 0;
+      }
+    },
+    onImpact: function (entity, force) {
+      if (entity.name() === "negative-charge") {
+        this.applyImpulse(60);
+      }
+      if (entity.name() === "target") {
+        this._world._destroy(this);
+      }
+      if (entity.name() === "ground") {
+        this._world._destroy(this);
+      }
+    },  
+  });
+
+  const block = {
+    name: "target",
+    shape: "square",
+    type: "static",
+    color: "pink",
+    width: 4,
+    height: 0.4,
+    onImpact: function (entity, force) {
+      if (entity.name() === "projectile") {
+        this.color("green");
+      }
+    },
+  };
+
+  ourWorld.createEntity(block, {
+    y: 12,
+    x: 20
+  });
+}
+
+canvas.addEventListener("click", function(event) { 
+    var userX = event.clientX / offsetX;
+    var userY = event.clientY / offsetY;
+    placeCharge(userX, userY);
+});
+
+function placeCharge(userX, userY) {
+  ourWorld.createEntity({
+    name: "negative-charge",
     shape: "square",
     type: "static",
     color: "purple",
     width: 3,
     height: 0.5,
-    x: 10,
-    y: 12,
+    x: userX,
+    y: userY
   })
+  console.log(userX);
+  console.log(userY);
 }
-
-ourWorld.createEntity(block, {
-  y: 12,
-  x: 20
-})
